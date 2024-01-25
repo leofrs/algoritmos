6deac50d9fc552cb4896a424f936d691cabfca2c
leonardo freire <leofrrodrigues86@gmail.com>
2024-01-25 11:13:27 -0300

    Lista atualizada até o sétimo exercício

--- basico/index.ts
+++ basico/index.ts
@@ -1,5 +1,5 @@
 /*
-Soma de Dois Números:
+1 - Soma de Dois Números:
 Escreva um programa que solicita ao usuário dois números e exibe a soma deles.
 */
 
@@ -10,7 +10,7 @@ const soma = (numero1: number, numero2: number): number => {
 //console.log("A soma dos números é:", soma(2, 2));
 
 /*
-Média de Três Números:
+2 - Média de Três Números:
 Crie um algoritmo que recebe três números do usuário e calcula a média.
 */
 
@@ -20,7 +20,8 @@ const media = (num1: number, num2: number, num3: number) => {
 
 //console.log("A média dos 3 números é:", media(5, 5, 5));
 
-/*Número Par ou Ímpar:
+/*
+3 - Número Par ou Ímpar:
 Escreva um programa que verifica se um número é par ou ímpar.
  */
 
@@ -34,7 +35,8 @@ const parOuImpar = (num: number) => {
 
 //parOuImpar(2);
 
-/*Calculadora Simples:
+/*
+4 - Calculadora Simples:
 Implemente uma calculadora simples que realiza operações de adição, subtração, multiplicação e divisão. O usuário deve inserir dois números e a operação desejada.
 */
 
@@ -53,3 +55,44 @@ class Calculadora {
     return console.log("A divisão dos números é:", num1 / num2);
   }
 }
+
+/*
+5 - Fatorial de um Número:
+Faça um algoritmo que calcula o fatorial de um número fornecido pelo usuário.
+ */
+
+const fatorial = (num: number): number => {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * fatorial(num - 1);
+  }
+};
+
+//console.log("O fatorial é:", fatorial(2));
+
+/*
+6 - Verificação de Número Primo:
+Escreva um programa que determina se um número informado pelo usuário é primo.
+*/
+
+const isPrimo = (number: number) => {
+  if (number % 2 === 1) {
+    return `O numero ${number} é primo `;
+  } else {
+    return `O numero ${number} não é primo `;
+  }
+};
+
+// console.log(isPrimo(5));
+
+/*
+7 - Conversão de Temperatura:
+Crie um programa que converte uma temperatura em graus Celsius para Fahrenheit. A fórmula é: Fahrenheit = (Celsius * 9/5) + 32. */
+
+const celsiusToFahrenheit = (celsius: number) => {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+};
+
+// console.log(`A temperatura em fahrenheit é: ${celsiusToFahrenheit(32)}°`);
